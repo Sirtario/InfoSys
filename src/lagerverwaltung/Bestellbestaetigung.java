dbe1d9d0676c4a7ce79528231325e24bcab3026b
max9076 <56943128+max9076@users.noreply.github.com>
2020-12-18 14:54:06 +0100

    Erstellung und Kommentare Bestellbestaetigung.java

--- src/lagerverwaltung/Bestellbestaetigung.java
+++ src/lagerverwaltung/Bestellbestaetigung.java
@@ -1,17 +1,39 @@
+package lagerverwaltung;
+
 public class Bestellbestaetigung {
 
-    boolean ausgefuehrt;
-    double gesamtpreis;
+    private boolean ausgefuehrt;
+    private double gesamtpreis;
 
+    /**
+     * Erstellung eines Bestellbestaetigung-Objektes:
+     * @param ausgefuehrt - Bestellbestätigung bereits gegeben?
+     * @param gesamtpreis - Gesamtpreis der Bestellung
+     */
     public Bestellbestaetigung(boolean ausgefuehrt, double gesamtpreis)
     {
-        private boolean isAusgefuehrt()
-        {
+        this.ausgefuehrt = ausgefuehrt;
+        this.gesamtpreis = gesamtpreis;
+    }
 
-        }
 
-        
+    /**
+     * Rückgabe, ob Bestellbestätigung
+     * @return von ausgefuehrt
+     */
+    public boolean isAusgefuehrt()
+    {
+        return ausgefuehrt;
+    }
 
+
+    /**
+     * Rückgabe des Gesamtpreises
+     * @return gesamtpreis
+     */
+    public double getGesamtpreis()
+    {
+        return gesamtpreis;
     }
 
 }
