39164d1299d5b064cceb4a238b5b6ea0e840ac2e
max9076 <56943128+max9076@users.noreply.github.com>
2020-12-30 16:37:13 +0100

    JavaDoc, Inline-Kommentare, Aufräumarbeiten, Organisation, kleine Fixes

--- src/lagerverwaltung/Lagerverwaltung.java
+++ src/lagerverwaltung/Lagerverwaltung.java
@@ -10,34 +10,34 @@ import java.util.Set;
 
 /**
  * @author Phillip Eckstein
+ * @author Maximilian Schumann
  *
+ * Die Lagerverwaltungs-Klasse nach den Vorgaben des InfoSys-Projekt
+ * WS 2020/2021
  */
 public class Lagerverwaltung {
 	
-	private Set<Mitarbeiter> berechtigteMitarbeiter;
-	private List<Lagerposten> lagerPosten;
-
+	private final Set<Mitarbeiter> berechtigteMitarbeiter;
+	private final List<Lagerposten> lagerPostenListe;
 	private final String dateiName;
 	//private PrintWriter writer;
 	//Wir benutzen diese Variable nicht, und erzeugen sie stattdessen in einer Methode, sobald geschrieben wird.
 
 
 	/**
-	 * Erstellt eine neue instanz der Lagerverwaltung
+	 * Erstellt eine neue instanz der Lagerverwaltung.
+	 * dateiName zum Loggen wird festgelegt.
 	 */
 	public Lagerverwaltung() {
-		berechtigteMitarbeiter = new HashSet<Mitarbeiter>();
-		lagerPosten = new ArrayList<Lagerposten>();
-		dateiName = "log.txt";
+		berechtigteMitarbeiter = new HashSet<>();
+		lagerPostenListe = new ArrayList<>();
+		dateiName = "log.txt"; //Datei, in der geloggt wird
 	}
-	
-	public Set<Mitarbeiter> getBerechtigteMitarbeiter() {
-		return berechtigteMitarbeiter;
-	}
-	
+
+
 	/**
-	 * Erteilt einem Mitarbeiter Berechtigung
-	 * @param arbeiter Der Mitarbeiter, der Berechtigung erhalten soll
+	 * Erteilt einem Mitarbeiter Berechtigung, die Lagerverwaltung nutzen zu dürfen.
+	 * @param arbeiter Der Mitarbeiter, der Befugnisse erhalten soll.
 	 */
 	public void BerechtigungErteilen(Mitarbeiter arbeiter)
 	{
@@ -49,53 +49,53 @@ public class Lagerverwaltung {
 
 
 	/**
-	 * Entzieht einem Mitarbeiter die Berechtigung
-	 * 
-	 * @param arbeiter Der Mitarbeiter der die berechtigung engtzogen bekommen soll
+	 * Entzieht einem Mitarbeiter die Rechte auf die Lagerverwaltung.
+	 * @param arbeiter Der Mitarbeiter der die berechtigung entzogen bekommen soll.
 	 */
 	public void BerechtigungEntziehen(Mitarbeiter arbeiter) 
 	{
-		if (arbeiter.getName() == null || arbeiter.getId() == null) throw new IllegalArgumentException("Name oder Id ist null");
+		if (arbeiter.getName() == null || arbeiter.getId() == null) throw new IllegalArgumentException("Name oder ID ist null");
 
 		if (!berechtigteMitarbeiter.remove(arbeiter)) throw new IllegalArgumentException("Mitarbeiter not found");
 		
 		SchreibNachrichtInDatei("Berechtigung entzogen für "+ arbeiter.getName());
 	}
-	
+
+
 	/**
-	 * Gibt den Lagerbestand in die standart konsole aus
+	 * Gibt den aktuellen Lagerbestand in die Standardkonsole aus.
 	 */
 	public void LagerbestandAusgeben() 
 	{
-		for(Lagerposten posten : lagerPosten) 
+		for(Lagerposten posten : lagerPostenListe)
 		{
-			System.out.println("Artikelname: "+ posten.getArtikel().getName()+" Stückzahl: "+posten.getLagerbestand()+" Preis: "+ posten.getPreis());
+			System.out.println("Artikelname: " + posten.getArtikel().getName()+ " Stückzahl: " +posten.getLagerbestand()+ " Preis: " + posten.getPreis());
 		}
 	}
-	
+
+
 	/**
 	 * Bucht einen Wareneingang. 
-	 * Wenn es schon Posten mit dem artikel gibt wird der alte preis mit dem neuen Preis überschrieben
+	 * Wenn es bereits einen Lagerposten mit diesem Artikel gibt, wird der alte Preis mit dem neuen Preis überschrieben.
 	 * @param arbeiter Der Mitarbeiter, der die Buchung vornimmt
-	 * @param art Der aertikel, der gebucht wird
+	 * @param artikel Der Artikel, der eingebucht wird
 	 * @param anzahl die Stückzahl des Artikels
-	 * @param preis Der neue Preis
+	 * @param preis Der Preis des Artikels
 	 */
-	public void WareneingangBuchen(Mitarbeiter arbeiter, Artikel art, int anzahl, double preis) 
+	public void WareneingangBuchen(Mitarbeiter arbeiter, Artikel artikel, int anzahl, double preis)
 	{
+		//Wareneingang nur zulässig, wenn ein berechtiger Mitarbeiter dies ausführt
 		if(berechtigteMitarbeiter.contains(arbeiter)) 
 		{
-			var posten = new Lagerposten(art, anzahl, preis);
+			var posten = new Lagerposten(artikel, anzahl, preis);
 			AddToLagerPosten(posten);
-			SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName()+" hat waren eingebucht:" +art.getName()+" Anzahl: "+anzahl+" Preis:"+preis);
+			SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName() + " hat Ware eingebucht: " + "ID: " + artikel.getId() + ", " + artikel.getName()+ " Anzahl: " +anzahl+ " Preis: " + preis + "€");
 		}
-		
 	} 
 
 	
 	/**
- 	* Führt eine Bestellung aus und entfernt die Posten aus dem Lager
- 	*
+ 	* Führt eine Bestellung aus und ändert/entfernt den/die Lagerposten.
  	* Bestellung kann nur ausgeführt werden, wenn ArtikelID dem System überhaupt bekannt ist UND Lagerbestand >= Bestellpostenmenge
  	* @param arbeiter Der Mitarbeiter, der die bestellung bearbeitet
  	* @param besPostenListe Liste von Posten, die bestellt sind
@@ -103,9 +103,11 @@ public class Lagerverwaltung {
  	*/
 	public Bestellbestaetigung BestellungAusfuehren(Mitarbeiter arbeiter, List<Bestellposten> besPostenListe)
 	{
-		if (lagerPosten.size() == 0) throw new IllegalArgumentException("LagerPosten ist leer!");
+		if (lagerPostenListe.size() == 0) throw new IllegalArgumentException("LagerPosten ist leer!");
 		//Ohne vorhandene Lagerposten kann auch keine Bestellung ausgeführt werden!
+
 		if (besPostenListe.size() == 0) throw new IllegalArgumentException("Bestellpostenliste ist leer!");
+		//Ohne vorhandene Bestellposten kann keine Bestellung ausgeführt werden!
 
 		if (!berechtigteMitarbeiter.contains((arbeiter)))
 		{
@@ -115,43 +117,39 @@ public class Lagerverwaltung {
 
 		float gesamtpreis = 0;
 
-
 		for (Bestellposten besPosten : besPostenListe) //besPostenListe enthält mehrere Bestellposten, die abzuarbeiten sind
 		{
-			for (Lagerposten lagPosten : lagerPosten) //Finden eines Lagerpostens, der die ArtikelID hat, die auch der aktuelle Bestellposten sucht
+			for (Lagerposten lagPosten : lagerPostenListe) //Finden eines Lagerpostens, der die ArtikelID hat, die auch der aktuelle Bestellposten sucht
 			{
 				if (lagPosten.getArtikel().getId().equals(besPosten.getArtikelId())) //Hat Artikel des Bestellpostens gleiche Artikel wie aktueller Lagerposten?
 				{
-					if (lagPosten.getLagerbestand() >= besPosten.getAnzahl()) //Gibt es von Artikel mehr Bestand, als Bestellposten verlangt?
+					if (lagPosten.getLagerbestand() >= besPosten.getAnzahl()) //Gibt es von Artikel mehr oder genau so viel Bestand, als Bestellposten verlangt?
 					{
 						lagPosten.setLagerbestand(lagPosten.getLagerbestand() - besPosten.getAnzahl()); //Lagerpostenbestand wird um Anzahl des Bestellpostens verringert
 						gesamtpreis += lagPosten.getPreis() * besPosten.getAnzahl();
-						break;
+						if (lagPosten.getLagerbestand() == 0) { lagerPostenListe.remove(lagPosten); } //Ist der Bestand eines Lagerpostens 0, gibt es diesen effektiv nicht mehr
+						break; //Damit, bei erfolgreichem Abziehen, nicht unnötig nach weiteren Übereinstimmungen der ArtikelID gesucht wird
 					} else
 					{
 						SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName()+ " hat erfolglos Bestellung ausgeführt, Fehler: ArtikelID " + besPosten.getArtikelId() + " zu wenig im Lager.");
 						return new Bestellbestaetigung(false, 0);
-						//Bestellposten konnte nicht durchgeführt werden, da zu wenig Bestand vorhanden ist
+						//Bestellposten konnte nicht verarbeitet werden, da zu wenig Bestand vorhanden ist
 					}
-				} else if (lagPosten == lagerPosten.get(lagerPosten.size()-1))
+				} else if (lagPosten == lagerPostenListe.get(lagerPostenListe.size()-1)) //Nur, wenn das gesamte Lager durchsucht worden ist, sind Fehlschläge erlaubt
 				{
-					SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName()+ " hat erfolglos Bestellung ausgeführt, Fehler: ArtikelID " + besPosten.getArtikelId() + " System nicht bekannt.");
+					SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName() + " hat erfolglos Bestellung ausgeführt, Fehler: ArtikelID " + besPosten.getArtikelId() + " System nicht bekannt.");
 					return new Bestellbestaetigung(false, 0);
 					//Ist auch nur irgendeine ArtikelID dem System aktuell unbekannt, bricht der gesamte Bestellposten ab
-
 				}
 			}
 		}
-
 
 		//Bestellung ist erfolgreich gewesen und jetzt wird geloggt, welche Artikel rausgingen
 		//StringBuilder Vorschlag von IDE; soll laut Recherche wohl bessere Performanz aufweisen und weniger fehleranfällig sein
 		StringBuilder IDs = new StringBuilder();
 		IDs.append(besPostenListe.get(0).getArtikelId());
-		if (besPostenListe.size() == 1)
-		{
-			IDs = new StringBuilder(besPostenListe.get(0).getArtikelId());
-		} else
+
+		if (besPostenListe.size() > 1)
 		{
 			for (int i = 1; i < besPostenListe.size(); i++)
 			{
@@ -159,8 +157,7 @@ public class Lagerverwaltung {
 			}
 		}
 
-
-		SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName() + " hat erfolgreich Bestellung ausgeführt. ArtikelIDs: " + IDs + ", Preis: " + gesamtpreis +"€");
+		SchreibNachrichtInDatei("Mitarbeiter " + arbeiter.getName() + " hat erfolgreich Bestellung ausgeführt. ArtikelIDs: " + IDs + ", Gesamtpreis: " + gesamtpreis +"€");
 		return new Bestellbestaetigung(true, gesamtpreis);
 		//Bestellung erfolgreich
 	}
@@ -174,33 +171,46 @@ public class Lagerverwaltung {
 	{
 		if(posten == null) throw new IllegalArgumentException("posten cannot be null");
 		
-		lagerPosten.add(posten);
+		lagerPostenListe.add(posten);
 	}
 
+
 	/**
-	 * Gibt die Lagerposten zurück
-	 * @return Liste mit Lagerposten
+	 * Gibt die Lagerposten als Liste zurück.
+	 * @return Liste mit Lagerposten-Objekten
 	 */
-	public List<Lagerposten> getLagerPosten() {
-		return lagerPosten;
+	public List<Lagerposten> getLagerPostenListe() {
+		return lagerPostenListe;
 	}
 
+
 	/**
-	 * Haengt eine Nachricht an die log Datei an
-	 * 
-	 * quelle: https://howtodoinjava.com/java/io/java-append-to-file/
-	 * @param msg die nachricht, die gelogt werden soll 
+	 * Gibt die berechtigten Mitarbeiter als Set zurück.
+	 * @return Set mit Mitarbeiter-Objekten
+	 */
+	public Set<Mitarbeiter> getBerechtigteMitarbeiter() {
+		return berechtigteMitarbeiter;
+	}
+
+
+	/**
+	 * Haengt eine Nachricht an die Log-Datei an.
+	 * Es wird automatisch vor die zu loggende Nachricht ein Timestamp angehangen im ISO8601-Format.; JJJJ-MM-SS hh:mm:ss.fff
+	 * Quelle: https://howtodoinjava.com/java/io/java-append-to-file/
+	 * @param msg die Nachricht, die geloggt werden soll.
 	 */
 	private void SchreibNachrichtInDatei(String msg)
 	{
+		//Try-Catch-Konstrukt, damit - sollte irgendein Fehler passieren - das Programm nicht abstürzt
 		try 
 		{
-			try (PrintWriter w = new PrintWriter(new FileWriter(dateiName, true)))
+			//Try-With-Resources-Konstrukt, damit die Ressource nach Nutzung wieder freigegeben wird
+			try (PrintWriter writer = new PrintWriter(new FileWriter(dateiName, true)))
 			{
-				w.println((new Timestamp(System.currentTimeMillis()).toString()+ " "+ msg));
+				writer.println((new Timestamp(System.currentTimeMillis()).toString()+ " "+ msg));
 			}
 		}
-		catch(Exception e)
+		catch(Exception e) //Exception e -> irgendeine Exception; es werden alle hier abgefangen
 		{
 			System.out.println("Could not write to file");
 		}
